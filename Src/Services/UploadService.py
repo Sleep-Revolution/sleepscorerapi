219dd52837886d5e2c014a0c8704e3eecccf42ae
Benedikt Hólm Þórðarson <benedikthth@gmail.com>
2023-06-23 19:26:42 +0000

    Update UploadService.py

--- Src/Services/UploadService.py
+++ Src/Services/UploadService.py
@@ -9,7 +9,7 @@ import pika
 import os 
 import json
 import requests
-from zbxsend import Metric, send_to_zabbix    
+from pyzabbix import ZabbixMetric, ZabbixSender
 
 UPLOAD_DIR = os.environ['DATA_ROOT_DIR']
 
@@ -114,14 +114,14 @@ class UploadService:
         zabbix_server = '130.208.209.7'
 
         # Create metrics
-        # metrics = [ZabbixMetric('sleepwell.sleep.ru.is', 'ESADA.upload', db_c.CentreName)]
+        metrics = [ZabbixMetric('sleepwell.sleep.ru.is', 'ESADA.upload', db_c.CentreName)]
 
         # Create a ZabbixSender instance
-        # zbx = ZabbixSender(zabbix_server)
+        zbx = ZabbixSender(zabbix_server)
 
         # Send metrics to zabbix
-        # zbx.send(metrics)
-        send_to_zabbix([Metric('sleepwell.sleep.ru.is', 'ESADA.upload',  db_c.CentreName)], zabbix_server, 10051)
+        zbx.send(metrics)
+        #send_to_zabbix([Metric('sleepwell.sleep.ru.is', 'ESADA.upload',  db_c.CentreName)], zabbix_server, 10051)
         #  This is going in another place!
         
     
